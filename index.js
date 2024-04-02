a59f59393bdc00627e7020057204df2c7f00832a
Mark Nyanjui <mark.warunge@student.moringaschool.com>
2024-04-02 14:36:11 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+function findTotalBatteries(){
+    return batteryBatches.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
+let totalBatteries = findTotalBatteries();
+
+console.log(totalBatteries)
